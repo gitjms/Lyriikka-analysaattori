0eadcdfaf62a8ef8ac0bab0ae78ca35332f7262e
gitjms <46410240+gitjms@users.noreply.github.com>
2020-05-14 23:26:12 +0300

    yritys deletoida

--- application/songs/models.py
+++ application/songs/models.py
@@ -6,9 +6,11 @@ from application import views
 class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(144), nullable=False)
-    text = db.Column(db.String(1000), nullable=False)
+    author = db.Column(db.String(144), nullable=False)
+    text = db.Column(db.String(2000), nullable=False)
 
-    def __init__(self, name, text):
+    def __init__(self, name, author, text):
         self.name = name
+        self.author = author
         self.text = text
 		
--- application/songs/views.py
+++ application/songs/views.py
@@ -1,6 +1,7 @@
 from application import app, db
-from flask import redirect, url_for, render_template, request
+from flask import redirect, url_for, render_template, request, flash
 from application.songs.models import Song
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 @app.route("/")
@@ -10,14 +11,19 @@ def songs_main():
 
 @app.route("/songs", methods=["GET", "POST"])
 def songs_index():
-	if request.form.get("Lyrics") == "Lyrics":
-		return render_template("songs/show.html", song = Song.query.get(str(songs_id)))
 	return render_template("songs/list.html", songs = Song.query.all())
 
-@app.route("/songs/show/<songs_id>/", methods=["GET", "POST"])
-def songs_show(songs_id):
-	return render_template("songs/show.html", song = Song.query.get(str(songs_id)))
+@app.route("/songs/show/<song_id>/", methods=["GET", "POST"])
+def songs_show(song_id):
+	return render_template("songs/show.html", song = Song.query.get(str(song_id)))
 
+
+@app.route("/songs", methods=["GET", "POST"])
+def songs_delete(song_id):
+	db.session().execute('DELETE FROM song WHERE id = ?'[int(song_id)])
+	db.session().commit()
+	flash('Song deleted')
+	return render_template("songs/list.html", songs = Song.query.all())
 
 @app.route("/songs/new/")
 def songs_form():
@@ -25,7 +31,7 @@ def songs_form():
 
 
 @app.route("/songs/", methods=["POST"])
-def songs_create():
+def song_create():
 	n = Song(request.form.get("name"),request.form.get("text"))
 
 	db.session().add(n)
