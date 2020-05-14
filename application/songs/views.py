fbb8008bcdffdb89bf321389f352c397cb81a603
gitjms <46410240+gitjms@users.noreply.github.com>
2020-05-14 18:04:49 +0300

    lyriikan näyttö, paluu mainiin

--- application/songs/views.py
+++ application/songs/views.py
@@ -3,14 +3,20 @@ from flask import redirect, url_for, render_template, request
 from application.songs.models import Song
 
 
-@app.route("/songs", methods=["GET"])
+@app.route("/")
+def songs_main():
+	return render_template("index.html")
+
+
+@app.route("/songs", methods=["GET", "POST"])
 def songs_index():
+	if request.form.get("Lyrics") == "Lyrics":
+		return render_template("songs/show.html", song = Song.query.get(str(songs_id)))
 	return render_template("songs/list.html", songs = Song.query.all())
 
-
-@app.route("/songs/show/<songs_id>/", methods=["GET"])
+@app.route("/songs/show/<songs_id>/", methods=["GET", "POST"])
 def songs_show(songs_id):
-	return render_template("songs/show.html", songs = Song.query.get(str(songs_id)))
+	return render_template("songs/show.html", song = Song.query.get(str(songs_id)))
 
 
 @app.route("/songs/new/")
