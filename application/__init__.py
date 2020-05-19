b941c276ce741baa228fc0972af4b51db8060281
gitjms <46410240+gitjms@users.noreply.github.com>
2020-05-19 11:57:58 +0300

    viilausta

--- application/__init__.py
+++ application/__init__.py
@@ -7,13 +7,13 @@ from flask_migrate import Migrate
 
 if os.environ.get("HEROKU"):
 	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-	# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-	# app.config["ENV"] = 'production'
+	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+	app.config["ENV"] = 'production'
 else:
 	app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///songs.db"
 	app.config["SQLALCHEMY_ECHO"] = True
-	# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-	# app.config["ENV"] = 'development'
+	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+	app.config["ENV"] = 'development'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -60,22 +60,82 @@ def insert_initial_accounts(*args, **kwargs):
 # 5 default songs
 #----------------------------------------------
 from application.songs.models import Song
-from pathlib import Path
 
 @event.listens_for(Song.__table__, 'after_create')
 def insert_initial_songs(*args, **kwargs):
-	for i in range(1,6):
-		file = 'static/default_songs/song'+str(i)+'.txt'
-		document_path = Path(__file__).parent / file
+	def song_1():
+		title = "Armo Jumalan"
+		author = "Jens Nicolai Ludvig Schjörring, H. S. Thompson"
+		lyrics = "Armo Jumalan\nlailla kirkkahan\nlähteen puuhtaan\nkuohuilee.\n\nSyvyydessä sen\narmolähtehen\nelon helmi\nhohtelee.\n\nArmo Jumalan\nlevon suloisan\nsydämehen\naina suo.\nJeesus-nimessä\nonnen löydät sä.\nTaivahan se\nmyötään tuo."
+	 
+		return title, author, lyrics
 
-		file = open(document_path, 'r', encoding='utf8')
-		title = file.readline().rstrip()
-		author = file.readline().rstrip()
-		lyrics = file.read()
+	def song_2():
+		title = "Ah, saavu, Jeesus"
+		author = "Abraham Achrenius, Toisinto Ylistarosta"
+		lyrics = "Ah, saavu, Jeesus,\nkeskellemme,\nniin sydämemme rukoilee.\nJo joutuu\nehtoo matkallemme,\njo armon päivä alenee.\nYö kohta peittää\nmaailman,\ntaisteluun käymme\nkuoleman.\n\nAh, kulje, Jeesus,\nrinnallamme,\nniin rakastamme,\nuskomme.\nSuo rohkeutta\nsodassamme,\nSinussa lepää toivomme.\nKun Jeesus\nseurassamme on,\non yökin päivä verraton.\n\nVain siitä syttyy\nsydämemme,\nkun saavut, Jeesus,\nkallehin,\nkirkastat armon\nriemuksemme,\ntaas vaikka\nkasvos salaatkin.\nTuntea saimme Jeesuksen,\nja ilo täyttää sydämen.\n\nIloiten sitten\nkiiruhdamme\nrientämään\nystäväimme luo\nja siellä\nheille julistamme:\nVieläkin avun Herra suo!\nMe luulimme,\non kuollut Hän,\nvaan näimme\nHänen elävän."
+	 
+		return title, author, lyrics
 
-		song = Song(title,author,lyrics)
-		song.account_id = 1
-		db.session.add(song)
-		db.session.commit()
+	def song_3():
+		title = "Ah, tulla jo Sun valtakuntasi suo"
+		author = "Lina Sandell-Berg, Gunr Wennerberg"
+		lyrics = "Ah, tulla jo Sun\nvaltakuntasi suo,\nja voimasi,\nHerra, Sä näytä.\nVie armosi viestiä\nkansojen luo\nja lähettis\ntulella täytä.\n|: Maan äärten suo\nkutsusi kuulla.:|\n\nSä siunaa ja\nvahvista palvelijas,\nmi kaukana\nlippua kantaa.\nSuo tuntea heidän\nSun rakkauttas\nja rauhaa,\nmi lohtua antaa,\n|: kun vaarojen\nteillä he käyvät.:|\n\nSuo voimasi heille\nSä taistelussaan\nja uskoa,\nuskollisuutta,\nniin että he toivossa\nkylvävät vaan\nja vartoovat\naikaasi uutta,\n|: kun korjata\nsadon Sä annat.:|\n\nJos kylvön he\nverellä kastella saa,\nniin armoa vain\non se Herran.\nNäin siunausta kasvaa\nvoi kuivakin maa,\nmi sadon myös\nkantavi kerran,\n|: ja tappio\nvoitoksi vaihtuu.:|"
+	 
+		return title, author, lyrics
+
+	def song_4():
+		title = "Ah, etten mitään oisi"
+		author = "Georgiana M. Taylor, Tuntematon"
+		lyrics = "Ah, etten mitään oisi,\ntyhjä, murrettu astia vaan,\npantu Mestarin\njalkain juureen\nkorjaustansa vartoamaan -\ntyhjä, Hän että voi täyttää,\nkun työtänsä\nkäyn tekemään,\nniin murrettu,\nettä voi käyttää\nmua syntistä etsimään.\n\nAh, etten mitään oisi,\nyksin Herraani tottelisin,\novenvartijan lailla valppaan\nHänen käskynsä täyttelisin -\nkanteleen\nsointuna helkkäin,\nkun Taitajan tahto on niin,\ntai vaieten,\nkunnes mun jälleen\nsaa sointuihin taivaisiin."
+	 
+		return title, author, lyrics
+
+	def song_5():
+		title = "Ajan myrskyaallot"
+		author = "Johnson Jr. Oatman, Edwin O. Excell"
+		lyrics = "Ajan myrskyaallot\nkun sua heittävät,\nsynkät pilvet usein\ntaivaan peittävät,\nmuista, että Jeesus\nmyrskyn hiljentää,\nepäuskon pilvetkin\nvoi hälventää.\n\n  Laske siunaukset\n  yksittäin,\n  huomaa Herran\n  hyvyys päivittäin.\n  Muista työnsä\n  suuret, valtavat,\n  uutta intoa ne\n  sulle antavat.\n\nPäivän helle matkalla\nkun ahdistaa,\nristi raskas painaa,\nHerra vahvistaa.\nSiunaukset\nkestävyyttä sulle tuo,\nPyhä Henki aina\nuutta voimaa suo.\n\nKun sä katsot\nrikkautta maailman,\nmuista: Jeesus antaa\naarteen suurimman.\nAnsaita ei kukaan\nsitä koskaan voi,\nlahjana sen\ntaivahasta Jeesus toi."
+	 
+		return title, author, lyrics
+
+	title = song_1()[0]
+	author = song_1()[1]
+	lyrics = song_1()[2]
+	song1 = Song(title,author,lyrics)
+	song1.account_id = 1
+	db.session.add(song1)
+	db.session.commit()
+
+	title = song_2()[0]
+	author = song_2()[1]
+	lyrics = song_2()[2]
+	song2 = Song(title,author,lyrics)
+	song2.account_id = 1
+	db.session.add(song2)
+	db.session.commit()
+
+	title = song_3()[0]
+	author = song_3()[1]
+	lyrics = song_3()[2]
+	song3 = Song(title,author,lyrics)
+	song3.account_id = 1
+	db.session.add(song3)
+	db.session.commit()
+
+	title = song_4()[0]
+	author = song_4()[1]
+	lyrics = song_4()[2]
+	song4 = Song(title,author,lyrics)
+	song4.account_id = 1
+	db.session.add(song4)
+	db.session.commit()
+
+	title = song_5()[0]
+	author = song_5()[1]
+	lyrics = song_5()[2]
+	song5 = Song(title,author,lyrics)
+	song5.account_id = 1
+	db.session.add(song5)
+	db.session.commit()
 
 db.create_all()
--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from application import app
 from flask import Flask
 
-app.add_url_rule('/images/<path:filename>', ...)
-
 if __name__ == "__main__":
 	#app.run()
 	app.run(debug=True)
