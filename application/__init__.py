379c1a649a2767ca0a9dfbc88265a1f355e7dccd
gitjms <46410240+gitjms@users.noreply.github.com>
2020-05-19 09:40:24 +0300

    init-viilausta

--- application/__init__.py
+++ application/__init__.py
@@ -7,13 +7,13 @@ from flask_migrate import Migrate
 
 if os.environ.get("HEROKU"):
 	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-	app.config["ENV"] = 'production'
+	# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+	# app.config["ENV"] = 'production'
 else:
 	app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///songs.db"
 	app.config["SQLALCHEMY_ECHO"] = True
-	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-	app.config["ENV"] = 'development'
+	# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+	# app.config["ENV"] = 'development'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
