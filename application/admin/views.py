e6fb2804d22021c016b5a9b45c454f0ed8957f1d
gitjms <46410240+gitjms@users.noreply.github.com>
2020-05-29 16:20:03 +0300

    heroku problems

--- application/admin/views.py
+++ application/admin/views.py
@@ -136,149 +136,146 @@ def clear():
 	elif request.form.get('add') == "add":
 
 		#------------------------------------------------
-		# check if tables are empty
-		#------------------------------------------------
-
-		if db.session.query(Author).first() is not None:
-			flash("Default authors and songs already exist.", "warning")
-			return render_template("admin/dashboard.html")
-
-		#------------------------------------------------
 		# add default authors
 		#------------------------------------------------
 
-		for item in authors.authors_fi:
-			song = item[1]
-			for auth in item[0]:
-				db.session.add(Author(name=auth))
-				db.session.commit()
+		if db.session.query(Author).first() is not None:
+			flash("Default authors already exist.", "warning")
+		else:
+			for item in authors.authors_fi:
+				song = item[1]
+				for auth in item[0]:
+					db.session.add(Author(name=auth))
+					db.session.commit()
 
-		for item in authors.authors_en:
-			song = item[1]
-			for auth in item[0]:
-				db.session.add(Author(name=auth))
-				db.session.commit()
-		
-		for item in authors.authors_fr:
-			song = item[1]
-			for auth in item[0]:
-				db.session.add(Author(name=auth))
-				db.session.commit()
+			for item in authors.authors_en:
+				song = item[1]
+				for auth in item[0]:
+					db.session.add(Author(name=auth))
+					db.session.commit()
+			
+			for item in authors.authors_fr:
+				song = item[1]
+				for auth in item[0]:
+					db.session.add(Author(name=auth))
+					db.session.commit()
 
 		#------------------------------------------------
 		# add default songs
 		#------------------------------------------------
 
 		fi_added = False
-
-		# finnish songs
-		for i in range(1,7):
-			document_path = os.getcwd()+'/application/static/default_songs/fi/song'+str(i)+'.txt'
-			file = open(document_path, 'r', encoding='utf-8')
-			title = file.readline().rstrip()
-			file.close()
-			lyrics = ""
-			with open(document_path, encoding='utf-8') as f:
-				for line in itertools.islice(f, 2, None):
-					lyrics += line
-			language = 'finnish'
-
-			song = Song(title,lyrics,language)
-			song.account_id = 1
-		
-			# if i == 1:
-				# song.authors.extend([Author.query.get(1),Author.query.get(2)])
-			# elif i == 2:
-				# song.authors.extend([Author.query.get(3),Author.query.get(4)])
-			# elif i == 3:
-				# song.authors.extend([Author.query.get(5),Author.query.get(6)])
-			# elif i == 4:
-				# song.authors.extend([Author.query.get(7),Author.query.get(8)])
-			# elif i == 5:
-				# song.authors.extend([Author.query.get(9),Author.query.get(10)])
-			# elif i == 6:
-				# song.authors.extend([Author.query.get(8),Author.query.get(11)])
-
-			try:
-				db.session.add(song)
-				db.session.commit()
-				fi_added = True
-			except:
-				db.session.rollback()
-
 		en_added = False
-		
-		# english songs
-		for i in range(7,13):
-			document_path = os.getcwd()+'/application/static/default_songs/en/song'+str(i)+'.txt'
-			file = open(document_path, 'r', encoding='utf-8')
-			title = file.readline().rstrip()
-			file.close()
-			lyrics = ""
-			with open(document_path, encoding='utf-8') as f:
-				for line in itertools.islice(f, 2, None):
-					lyrics += line
-			language = 'english'
-
-			song = Song(title,lyrics,language)
-			song.account_id = 1
-			
-			# if i == 7:
-				# song.authors.extend([Author.query.get(12)])
-			# elif i == 8:
-				# song.authors.extend([Author.query.get(9),Author.query.get(13)])
-			# elif i == 9:
-				# song.authors.extend([Author.query.get(14)])
-			# elif i == 10:
-				# song.authors.extend([Author.query.get(15),Author.query.get(16),Author.query.get(17)])
-			# elif i == 11:
-				# song.authors.extend([Author.query.get(18)])
-			# elif i == 12:
-				# song.authors.extend([Author.query.get(19)])
-
-			try:
-				db.session.add(song)
-				db.session.commit()
-				en_added = True
-			except:
-				db.session.rollback()
-
 		fr_added = False
+
+		if db.session.query(Song).first() is not None:
+			flash("Default songs already exist.", "warning")
+		else:
+
+			# finnish songs
+			for i in range(1,7):
+				document_path = os.getcwd()+'/application/static/default_songs/fi/song'+str(i)+'.txt'
+				file = open(document_path, 'r', encoding='utf-8')
+				title = file.readline().rstrip()
+				file.close()
+				lyrics = ""
+				with open(document_path, encoding='utf-8') as f:
+					for line in itertools.islice(f, 2, None):
+						lyrics += line
+				language = 'finnish'
+
+				song = Song(title,lyrics,language)
+				song.account_id = 1
 		
-		# french songs
-		for i in range(13,19):
-			document_path = os.getcwd()+'/application/static/default_songs/fr/song'+str(i)+'.txt'
-			file = open(document_path, 'r', encoding='utf-8')
-			title = file.readline().rstrip()
-			file.close()
-			lyrics = ""
-			with open(document_path, encoding='utf-8') as f:
-				for line in itertools.islice(f, 2, None):
-					lyrics += line
-			language = 'french'
+				if i == 1:
+					song.authors.extend([Author.query.get(1),Author.query.get(2)])
+				elif i == 2:
+					song.authors.extend([Author.query.get(3),Author.query.get(4)])
+				elif i == 3:
+					song.authors.extend([Author.query.get(5),Author.query.get(6)])
+				elif i == 4:
+					song.authors.extend([Author.query.get(7),Author.query.get(8)])
+				elif i == 5:
+					song.authors.extend([Author.query.get(9),Author.query.get(10)])
+				elif i == 6:
+					song.authors.extend([Author.query.get(8),Author.query.get(11)])
 
-			song = Song(title,lyrics,language)
-			song.account_id = 1
+				try:
+					db.session.add(song)
+					db.session.commit()
+					fi_added = True
+				except:
+					db.session.rollback()
+
+			# english songs
+			for i in range(7,13):
+				document_path = os.getcwd()+'/application/static/default_songs/en/song'+str(i)+'.txt'
+				file = open(document_path, 'r', encoding='utf-8')
+				title = file.readline().rstrip()
+				file.close()
+				lyrics = ""
+				with open(document_path, encoding='utf-8') as f:
+					for line in itertools.islice(f, 2, None):
+						lyrics += line
+				language = 'english'
+
+				song = Song(title,lyrics,language)
+				song.account_id = 1
 			
-			# if i == 13:
-				# song.authors.extend([Author.query.get(20)])
-			# elif i == 14:
-				# song.authors.extend([Author.query.get(21),Author.query.get(22)])
-			# elif i == 15:
-				# song.authors.extend([Author.query.get(23),Author.query.get(24)])
-			# elif i == 16:
-				# song.authors.extend([Author.query.get(21),Author.query.get(25)])
-			# elif i == 17:
-				# song.authors.extend([Author.query.get(26)])
-			# elif i == 18:
-				# song.authors.extend([Author.query.get(27)])
+				if i == 7:
+					song.authors.extend([Author.query.get(12)])
+				elif i == 8:
+					song.authors.extend([Author.query.get(9),Author.query.get(13)])
+				elif i == 9:
+					song.authors.extend([Author.query.get(14)])
+				elif i == 10:
+					song.authors.extend([Author.query.get(15),Author.query.get(16),Author.query.get(17)])
+				elif i == 11:
+					song.authors.extend([Author.query.get(18)])
+				elif i == 12:
+					song.authors.extend([Author.query.get(19)])
 
-			try:
-				db.session.add(song)
-				db.session.commit()
-				fr_added = True
-			except:
-				db.session.rollback()
+				try:
+					db.session.add(song)
+					db.session.commit()
+					en_added = True
+				except:
+					db.session.rollback()
+
+			# french songs
+			for i in range(13,19):
+				document_path = os.getcwd()+'/application/static/default_songs/fr/song'+str(i)+'.txt'
+				file = open(document_path, 'r', encoding='utf-8')
+				title = file.readline().rstrip()
+				file.close()
+				lyrics = ""
+				with open(document_path, encoding='utf-8') as f:
+					for line in itertools.islice(f, 2, None):
+						lyrics += line
+				language = 'french'
+
+				song = Song(title,lyrics,language)
+				song.account_id = 1
+			
+				if i == 13:
+					song.authors.extend([Author.query.get(20)])
+				elif i == 14:
+					song.authors.extend([Author.query.get(21),Author.query.get(22)])
+				elif i == 15:
+					song.authors.extend([Author.query.get(23),Author.query.get(24)])
+				elif i == 16:
+					song.authors.extend([Author.query.get(21),Author.query.get(25)])
+				elif i == 17:
+					song.authors.extend([Author.query.get(26)])
+				elif i == 18:
+					song.authors.extend([Author.query.get(27)])
+
+				try:
+					db.session.add(song)
+					db.session.commit()
+					fr_added = True
+				except:
+					db.session.rollback()
 
 		if fi_added:
 			flash("Finnish songs added.", "success")
