25744b86c4194adeaa8b6aecaf4410f61ecb77f5
gitjms <46410240+gitjms@users.noreply.github.com>
2020-05-30 23:45:09 +0300

    aggregate queries

--- application/words/models.py
+++ application/words/models.py
@@ -27,27 +27,32 @@ class Words(db.Model):
 	@staticmethod
 	def find_songs_authors_with_matches_geq_avg():
 		user_list = [g.user.id,1]
-		
+
 		stmt = text("SELECT"
-					"	results.word,"
-                    "	Author.name,"
-                    "	Song.name,"
+					"	DISTINCT results.word,"
                     "	results.matches,"
-                    "	SUM(results.matches) AS sum, "
-                    "	AVG(results.matches) AS avg "
-					"FROM account "
-					"JOIN Song ON account.id = Song.account_id "
-					"JOIN results ON Song.id = results.song_id "
-					"JOIN author_song ON Song.id = author_song.song_id "
-					"JOIN Author ON author_song.author_id = Author.id "
-					"WHERE account.id IN (:user1,:user2) "
-					"GROUP BY results.word, Song.name, results.matches, Author.name "
+                    "	COUNT(song.sum),"
+                    "	results.matches/COUNT(song.sum)"
+					"FROM results "
+					"JOIN Song ON Song.id = results.song_id "
+					"JOIN "
+					"( "
+					"	SELECT"
+					"		DISTINCT Song.account_id,"
+					"		results.matches AS matches,"
+					"		SUM(results.matches) AS sum,"
+					"		AVG(results.matches) AS avg"
+					"	FROM song"
+					"	JOIN account ON account.id = Song.account_id"
+					"	JOIN results ON results.song_id = Song.id"
+					"	WHERE account.id IN (:user1,:user2) "
+					") AS song "
+					"GROUP BY results.matches "
 					"ORDER BY results.matches DESC "
-					"LIMIT :limit OFFSET :offset").params(user1=user_list[0],user2=user_list[1],limit=5,offset=1)
+					"LIMIT :limit").params(user1=user_list[0],user2=user_list[1],limit=6)
 		res = db.engine.execute(stmt)
 		response = []
 		for row in res:
-			response.append({'word':row[0], 'author':row[1], 'title':row[2], 'matches':row[3], 'sum':row[4], 'average':row[5]})
+			response.append({'word':row[0], 'totcount':row[1], 'numsongs':row[2], 'average':row[3]})
 
 		return response
-
