5a362b52d762fc772f9109d6d29d541468d24f02
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-18 22:27:25 +0300

    Purge cache

--- application/poems/models.py
+++ application/poems/models.py
@@ -21,8 +21,8 @@ class Poem(Base):
 	lyrics = db.Column(db.Text, nullable=False)
 	language = db.Column(db.String(255), nullable=False)
 
-	account_id = db.Column(db.Integer, db.ForeignKey('account.id'), primary_key=True)
-	account_role = db.Column(db.Integer, db.ForeignKey('account.role'), primary_key=True)
+	account_id = db.Column(db.Integer, db.ForeignKey('account.id'), unique=True)
+	account_role = db.Column(db.Integer, db.ForeignKey('account.role'), unique=True)
 
 
 	def __init__(self, name, lyrics, language):
