56948dcaa0fe6d64cee5dab179b2b9d2675005af
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-18 15:41:58 +0300

    trial fix for adding users

--- application/auth/views.py
+++ application/auth/views.py
@@ -148,7 +148,7 @@ def auth_create():
 		db.session().add(user)
 		db.session().commit()
 		login_user(user)
-	except IntegrityError:
+	except:
 		db.session.rollback()
 		flash("Create account failed.", "danger")
 		return render_template("auth/newuser.html", form=form, error="User already exists. Consider changing username.")
