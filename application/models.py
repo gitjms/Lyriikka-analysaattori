8de3b654b00a2533da48158787f9431f4c09ffa0
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-18 20:40:27 +0300

    Purge cache

--- application/models.py
+++ application/models.py
@@ -5,4 +5,4 @@ class Base(db.Model):
 	__abstract__ = True
   
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(255), nullable=False, unique=True)
+	name = db.Column(db.String(255), nullable=False)
