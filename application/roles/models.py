f4e1aacde1810d8cdf9bfde4e2ded14a81a29ad2
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-18 21:43:39 +0300

    Purge cache

--- application/roles/models.py
+++ application/roles/models.py
@@ -6,17 +6,13 @@ class Role(db.Model):
 	__tablename__ = "roles"
 
 	id = db.Column(db.Integer, primary_key=True)
-	role = db.Column(db.String(255), primary_key=True)
+	role = db.Column(db.String(255), nullable=False)
 
 
-	def __init__(self, id, role):
-		self.id = id
+	def __init__(self, role):
 		self.role = role
 
 	def get_id(self):
 		return self.id
-
-	def get_role(self):
-		return self.role
 
 
