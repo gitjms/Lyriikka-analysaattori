6aef7afc033ed9d92e03dbfdea0f014ed4d7b347
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-19 03:58:54 +0300

    Recent 'Minor fixes'
    Okay, the recent so called 'Minor fixes' were about handling server
    error. When toggling fast between 'Stats' and 'List Users' pages as
    admin, the application crashed every now and then because of error
    code=H18 desc='Server Request Interrupted' - 'POST /admin/dashboard
    HTTP/1.1', 'https://lyrfreq.herokuapp.com/auth/stats/na'.
    
    The '/admin/dashboard' above is the same as 'List Users', and the https
    address is 'Stats'.
    
    I don't know exactly what is going on, but it kinda works now as I put a
    'GET' method to 'List Users' form call in application/layout.html.
    
    I'm not sure if this 'duct taping' will hold for long, but it will do
    for now.

--- application/admin/views.py
+++ application/admin/views.py
@@ -38,7 +38,7 @@ def before_request():
 def admin_dashboard():
 
 	users = User.query.all()
-	return redirect(url_for("admin/dashboard.html", users=users))
+	return render_template("admin/dashboard.html", users=users)
 
 
 #----------------------------------------------------
